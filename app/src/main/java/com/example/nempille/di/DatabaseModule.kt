f995034c9b9905d676a3e6f3436512dc983133f7
Alina <alikr23@student.sdu.dk>
2025-12-02 15:58:25 +0100

    Hilt setup is done (added DatabaseModule and RepositoryModule)

--- app/src/main/java/com/example/nempille/di/DatabaseModule.kt
+++ app/src/main/java/com/example/nempille/di/DatabaseModule.kt
@@ -1,4 +1,38 @@
 package com.example.nempille.di
 
-class DatabaseModule {
+import android.content.Context
+import androidx.room.Room
+import com.example.nempille.data.local.database.AppDatabase
+import com.example.nempille.data.local.dao.MedicationDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+//this class tells Hilt HOW to create database-related objects
+@Module //class defines bindings for DI
+@InstallIn(SingletonComponent::class)//available app-wide (singleton scope)
+object DatabaseModule{
+    //provide a single instance of AppDatabase for the whole app
+    @Provides
+    @Singleton //only one db instance
+    fun provideAppDatabase(
+        @ApplicationContext context: Context
+    ): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "nempille_db" //name of the sqlite file
+        ).build()
+    }
+
+    //Provide DAO. Hilt knows how to get AppDatabase from the fun above
+    @Provides
+    fun provideMedicationDao(
+        database: AppDatabase
+    ): MedicationDao {
+        return database.medicationDao()
+    }
 }
--- app/src/main/java/com/example/nempille/di/RepositoryModule.kt
+++ app/src/main/java/com/example/nempille/di/RepositoryModule.kt
@@ -1,4 +1,25 @@
 package com.example.nempille.di
 
-class RepositoryModule {
+import com.example.nempille.data.local.dao.MedicationDao
+import com.example.nempille.data.repository.MedicationRepositoryImpl
+import com.example.nempille.domain.repository.MedicationRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+//module that binds interfaces (domain) to implement (data)
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+    //provide singleton MedicationRepository
+    @Provides
+    @Singleton
+    fun provideMedicationRepository(
+        dao: MedicationDao
+    ): MedicationRepository {
+        //hilt already knows how to get MedicationDao (from DatabaseModule)
+        return MedicationRepositoryImpl(dao)
+    }
 }
